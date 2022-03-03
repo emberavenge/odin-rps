6cb8e9999e894735e2bb1f75d1cefff4b4bacc3c
Emerson Castillo <emberavenge@protonmail.com>
2022-03-03 00:15:15 -0400

    Added final touches

--- app.js
+++ app.js
@@ -1,43 +1,31 @@
-// Rock definition
-
 const rock = document.querySelector("#rock");
-
-rock.addEventListener("click", playRound(playerRock));
-
-function playerRock() {
-  return "rock";
-}
-
-// Paper definition
-
-const paper = document.querySelector("#papers");
-
-paper.addEventListener("click", playRound(playerPaper));
-
-function playerPaper() {
-  return "paper";
-}
-
-// Scissors definition
-
+const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
-
 const scoreboardDiv = document.querySelector("#scoreboard");
+const score = document.querySelector("#score");
+const cpuScore = document.querySelector("#cpuScore");
 
-const playerScore = document.createElement("p");
+rock.addEventListener("click", (e) => {
+  e.preventDefault();
 
+  playRound("rock", computerPlay());
+});
 
+paper.addEventListener("click", (e) => {
+  e.preventDefault();
 
-scissors.addEventListener('click', playRound(playerScissors));
+  playRound("paper", computerPlay());
+});
 
-function playerScissors() {
-return "scissors";
+scissors.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  playRound("scissors", computerPlay());
+});
+
+function addScore(elem) {
+  elem.innerText = parseInt(elem.innerText) + 1;
 }
-
-
-//
-
-
 
 function computerPlay() {
   let computerChoice = ["Rock", "Paper", "Scissors"];
@@ -46,42 +34,26 @@ function computerPlay() {
   return computerSelection;
 }
 
-
 function playRound(playerSelection, computerSelection) {
-  let playerPoints = 0
-  let computerPoints = 0;
-
-  // switch(playerSelection === "rock" && computerSelection === "Scissors") {
-  //   case 1:
-  //     playerSelection++;
-  // }
-
-  if ( playerPoints && computerPoints !== 5) {
-    if (playerSelection === "rock" && computerSelection === "Scissors") {
-      return playerPoints++;
-    } else if (playerSelection === "rock" && computerSelection === "Paper") {
-      return computerPoints++;
-    } else if (playerSelection === "rock" && computerSelection === "Rock") {
-      alert("It's a tie between player and computer");
-    } else if (playerSelection === "paper" && computerSelection === "Scissors") {
-      return computerPoints++;
-    } else if (playerSelection === "paper" && computerSelection === "Rock") {
-      return playerPoints++;
-    } else if (playerSelection === "paper" && computerSelection === "Paper") {
-      alert("It's a tie between player and computer!");
-    } else if (
-      playerSelection === "scissors" &&
-      computerSelection === "Scissors"
-    ) {
-      alert("It's a tie between player and computer");
-    } else if (playerSelection === "scissors" && computerSelection === "Rock") {
-      alert("You lose! Scissors loses to Rock");
-    } else if (playerSelection === "scissors" && computerSelection === "Paper") {
-      return playerPoints++;
-    }
+  if (playerSelection === "rock" && computerSelection === "Scissors") {
+    addScore(score);
+  } else if (playerSelection === "paper" && computerSelection === "Rock") {
+    addScore(score);
+  } else if (playerSelection === "scissors" && computerSelection === "Paper") {
+    addScore(score);
+  } else if (computerSelection === "Paper" && playerSelection === "rock") {
+    addScore(cpuScore);
+  } else if (computerSelection === "Rock" && playerSelection === "scissors") {
+    addScore(cpuScore);
+  } else if (computerSelection === "Scissors" && playerSelection === "paper") {
+    addScore(cpuScore);
+  } else if (playerSelection === computerSelection) {
+    return "Ït's a tie.";
   }
-    if (playerPoints === 5) {
-      playerScore.textContent = `Player wins with${playerPoints}`;
-      scoreboardDiv.appendChild(playerScore);
-    }
+
+  if (parseInt(score.innerText) >= 5) {
+    alert("Player wins");
+  } else if (parseInt(cpuScore.innerText) >= 5) {
+    alert("CPU wins");
+  }
 }
